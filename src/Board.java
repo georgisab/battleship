92b8c355f93be92624689fc68e6b2e0f13102b71
georgisab <georgisab@hotmail.com>
2015-02-25 09:27:00 +0200

    test2

--- src/Board.java
+++ src/Board.java
@@ -10,7 +10,7 @@ public class Board {
 			}
 		}
 	}
-	//Рисува Матрицата Paint matrix 
+	// Paint matrix 
 	public void writeMatrix() {
 		for (int row = 0; row < board.length; row++) {
 			for (int col = 0; col < board[0].length; col++) {
